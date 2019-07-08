c589bc1d8d33454268ee2ed147af0903232efd95
Ben Andrew <benjamin.paul.andrew@gmail.com>
2019-07-08 15:33:13 +1000

    ..

---
+++ app.py
@@ -0,0 +1,93 @@
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+print(db)
+from models import Book, User
+
+@app.route("/")
+def hello():
+    return "Hello World!!"
+
+@app.route("/add")
+def add_book():
+    name=request.args.get('name')
+    author=request.args.get('author')
+    published=request.args.get('published')
+    try:
+        book=Book(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return "Book added. book id={}".format(book.id)
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/getall")
+def get_all():
+    try:
+        books=Book.query.all()
+        return  jsonify([e.serialize() for e in books])
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book=Book.query.filter_by(id=id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+	    return(str(e))
+
+
+@app.route("/users")
+def users():
+    try:
+        users=User.query.all()
+        return  jsonify([e.serialize() for e in users])
+    except Exception as e:
+	    return(str(e))
+
+
+@app.route("/users/add")
+def users_add():
+    first_name=request.args.get('first_name')
+    last_name=request.args.get('last_name')
+    email=request.args.get('email')
+    try:
+        user=User(
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+        db.session.add(user)
+        db.session.commit()
+        return "User added. user id={}".format(user.id)
+    except Exception as e:
+	    return(str(e))
+
+
+@app.route("/users/delete/<id_>")
+def users_delete(id_):
+    try:
+        obj=User.query.filter_by(id=id_).one()
+        db.session.delete(obj)
+        db.session.commit()
+        return "User deleted. user id={}".format(id_)
+    except Exception as e:
+	    return(str(e))
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,49 @@
+from app import db
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    published = db.Column(db.String())
+
+    def __init__(self, name, author, published):
+        self.name = name
+        self.author = author
+        self.published = published
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'name': self.name,
+            'author': self.author,
+            'published':self.published
+        }
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    email = db.Column(db.String())
+
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email':self.email
+        }
