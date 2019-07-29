cdf6b0c3f6cf9c440f06c6c01c481e2a5765aec7
Ben Andrew <benjamin.paul.andrew@gmail.com>
2019-07-29 19:41:39 +1000

    ..

--- app.py
+++ app.py
@@ -625,7 +625,7 @@ def unspsc():
 
         result = unspscs_schema.dump(unspsc).data  # THIS SHOWS ALL OF THE Ops FOR THE UNSPSCs
         #result = unspscs_simple_schema.dump(unspsc).data
-        return json.dumps(result)
+        #return json.dumps(result)
         return jsonify(result)
     except Exception as e:
 	    return(str(e))
